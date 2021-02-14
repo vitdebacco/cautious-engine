abb274d997a159923bd45eaef500d308f6b102c8
Vit DeBacco <vdebacco@gmail.com>
2021-02-14 12:40:12 -0500

    feat: initial Spring Boot implementation (#5)
    This commit adds the start of the Spring Boot (and Postgres, eventually)
    implementation of cautious-engine.
    
    The initial phase exposes two endpoints:
    GET /addresses
    Returns an array containing 0-100 randomly generated addresses.
    
    GET /addresses/:id
    Returns a single randomly generated address. The `:id` parameter can be any
    combination of valid characters, as it is ignored and only used for routing.

---
+++ spring-boot_postgres/src/main/kotlin/com/envylabs/cautiousengine/CautiousengineApplication.kt
@@ -0,0 +1,11 @@
+package com.envylabs.cautiousengine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CautiousengineApplication
+
+fun main(args: Array<String>) {
+    runApplication<CautiousengineApplication>(*args)
+}
---
+++ spring-boot_postgres/src/main/kotlin/com/envylabs/cautiousengine/config/JacksonConfig.kt
@@ -0,0 +1,29 @@
+package com.envylabs.cautiousengine.config
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JacksonConfig {
+
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper().apply {
+            this.propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+            this.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            this.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            this.registerModule(JavaTimeModule()) // for JSR310
+            this.registerModule(
+                KotlinModule(
+                    nullToEmptyCollection = true,
+                    nullToEmptyMap = true,
+                    nullIsSameAsDefault = true
+                )
+            )
+        }
+    }
+}
