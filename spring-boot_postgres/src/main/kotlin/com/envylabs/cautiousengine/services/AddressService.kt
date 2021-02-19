f376a86eff8d3d4de2cc687d33c8e23328ffec1b
vitdebacco <vit.debacco@validic.com>
2021-02-18 21:02:12 -0500

    fix: lower `/addresses` limit to 10 (from 100)
    This commit lowers the max number of results returned by `/addresses` from 100
    to 10. A max of 100 was creating unrealistic memory requirements for the load
    tests running with 1,000 concurrent users.

--- spring-boot_postgres/src/main/kotlin/com/envylabs/cautiousengine/services/AddressService.kt
+++ spring-boot_postgres/src/main/kotlin/com/envylabs/cautiousengine/services/AddressService.kt
@@ -3,6 +3,6 @@ package com.envylabs.cautiousengine.services
 import com.envylabs.cautiousengine.models.Address
 
 interface AddressService {
-    fun many(limit: Int = 100): List<Address>
+    fun many(limit: Int = 10): List<Address>
     fun one(): Address
 }
